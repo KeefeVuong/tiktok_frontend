48db8338fa8b8a7394f40a4db3d02c26998c133d
Your Name <keefevuong123@gmail.com>
2023-09-14 19:18:36 +1000

    updated url

--- src/Helper.jsx
+++ src/Helper.jsx
@@ -14,7 +14,7 @@ export const APIFetch = async (path, method, body) => {
   }
 
   try {
-    const response = await fetch(`http://192.9.172.61${path}`, callContent);
+    const response = await fetch(`https://www.keefe-tk-be.xyz${path}`, callContent);
     // const response = await fetch(`http://127.0.0.1:8000${path}`, callContent);
     const data = await response.json();
     if (response.ok) {
